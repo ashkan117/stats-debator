71706248e29d0b756ef2a2b58327b5d81b2beb92
Parham Hajzavar <39533157+pzavar@users.noreply.github.com>
2020-02-24 11:19:51 -0800

    cleaned teams.js

--- gen/server/src/routes/teams.js
+++ gen/server/src/routes/teams.js
@@ -2,8 +2,6 @@ var express = require('express');
 const NBA = require("nba");
 var router = express.Router();
 
-//const rocketsId = NBA.teamIdFromName('Rockets');
-
 //:teamName maps the first paramater to have the name teamName
 //All parameters are in req.param
 //So req.params.teamName is the first parameter
@@ -17,20 +15,5 @@ router.get('/:teamName', (req,res) => {
         });
     }
 });
-
-// app.get('/shmrk/product/:product_id', productRoutes. getProductById);
-
-
-//console.log(rockets);
-/* logs the following:
-{
-  firstName: 'Stephen',
-  lastName: 'Curry',
-  playerId: 201939,
-  teamId: 1610612744,
-  fullName: 'Stephen Curry',
-  downcaseName: 'stephen curry'
-}
-*/
 
 module.exports = router;
