fa6ab236dcfb0f4f0be60e24117a491f754fd93a
Ashkan Faghihi <ashkan117@yahoo.com>
2020-02-23 18:08:28 -0800

    working connection with backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:stats_debator/components/standings.dart';
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -8,12 +10,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    HttpClient client = new HttpClient();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Standings(),
+      home: FutureBuilder(
+//          future: client
+//              .getUrl(Uri.parse("http://172.28.172.226:8000/teams/Rockets")),
+          future: http.get("http://172.28.172.226:8000/teams/Rockets"),
+          builder:
+              (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
+            if (snapshot.hasData &&
+                snapshot.connectionState != ConnectionState.waiting) {
+              return Text(jsonDecode(snapshot.data.body).toString());
+            } else {
+              return Text("ERROR");
+            }
+          }),
     );
   }
 }
